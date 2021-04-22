2ba0c64e1b917f81b467ad14031d29c9dbcaf69b
‘admahood’ <admahood@gmail.com>
2021-04-22 11:52:42 -0600

    k

--- make_dissertation.R
+++ make_dissertation.R
@@ -3,5 +3,5 @@ library(rmarkdown)
 rmarkdown::render("dissertation_mahood.Rmd")
 rmarkdown::render("dissertation_title.Rmd")
 
-system(paste("pdftk dissertation_title.pdf dissertation_mahood.pdf cat output full_dissertation.pdf"))
+system(paste("pdftk dissertation_title.pdf taf.pdf dissertation_mahood.pdf cat output full_dissertation.pdf"))
 
