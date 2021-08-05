7872aa36b1b04bd83a70ca8d23a0cadd7308a71f
Lisa Rosenthal <lrosenthal@ucdavis.edu>
2021-08-04 18:48:07 -0700

    wrote abstract. also now append the title page within the rmd file, so no need to have a seperate make_dissertation script.

--- make_dissertation.R
+++
@@ -1,7 +0,0 @@
-library(pdftools)
-
-#first knit the dissertation 
-
-#combine with title page
-pdf_combine(c("title_page.pdf", "dissertation_Lrosenthal.pdf"), output = "full_dissertation_LRosenthal.pdf")
-
